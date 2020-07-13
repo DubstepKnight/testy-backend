2dc4aabf6021ad29ee2f3b3913a3705efe8b0770
DubstepKnight <nursmaster@gmail.com>
2020-07-13 21:22:29 +0300

    fixed a small issue

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port: ${port}!`);
+    console.log(`Server is running on port: ${PORT}!`);
 })
